7bce492248875e72ec602d9760a27a27f57f751f
fsajadi <s.sfatemeh@gmail.com>
2020-06-23 18:02:18 -0400

    3_queries_group-by

--- 3_queries_group-by/6.sql
+++ 3_queries_group-by/6.sql
@@ -1,4 +1,8 @@
-SELECT students.name, AVG(assignment_submissions.duration), AVG(assignment_submissions.duration)
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
 FROM students
-JOIN assignment_submissions ON students.id=assignment_submissions.student_id
-GROUP BY students.name,assignment_submissions.assignment_id;
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
